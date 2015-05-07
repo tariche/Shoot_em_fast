9101fb1e4cc48464fe1442f276b360860f00a908
Tarik Zunic <tarik.zunic@gmail.com>
2015-05-08 00:28:22 +0200

    Created: MainMenuScreen Initial: HelpScreen HighScoreScreen GameScreen

--- app/src/main/java/com/tarikzunic/shootemfast/HelpScreen.java
+++ app/src/main/java/com/tarikzunic/shootemfast/HelpScreen.java
@@ -1,7 +1,11 @@
 package com.tarikzunic.shootemfast;
 
 import com.tarikzunic.framework.Game;
+import com.tarikzunic.framework.Graphics;
+import com.tarikzunic.framework.Input.TouchEvent;
 import com.tarikzunic.framework.Screen;
+
+import java.util.List;
 
 /**
  * Created by tarik on 5/7/15.
@@ -13,12 +17,24 @@ public class HelpScreen extends Screen {
 
     @Override
     public void update(float deltaTime) {
-
+        List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
+        int len = touchEvents.size();
+        for (int i = 0; i < len; i++) {
+            TouchEvent event = touchEvents.get(i);
+            if (event.type == TouchEvent.TOUCH_UP) {
+                if (event.x < 80 && event.y < 80) {
+                    game.setScreen(new ManiMenuScreen(game));
+                }
+            }
+        }
     }
 
     @Override
     public void present(float deltaTime) {
+        Graphics g = game.getGraphics();
 
+        g.drawPixmap(Assets.background, 0, 0);
+        g.drawPixmap(Assets.buttons, 0, 0, 80, 80, 80, 80);
     }
 
     @Override
